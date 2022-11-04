8dd694b7bc6818daa67ee0b175794283a09dcb6f
turaneligul <91722837+turaneligul@users.noreply.github.com>
2022-11-04 22:14:51 +0300

    rent a car
    rent a car

---
+++ rentACar1/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,26 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Table(name="brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+}
+
+//Brand--> id,name,quantity
+//GetAllBrandsResponse-->id,name
+//mapping
---
+++ rentACar1/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -0,0 +1,32 @@
+package kodlama.io.rentACar.webApi.controllers;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController //annotation
+@RequestMapping("/api/brands")
+public class BrandsController {
+
+    private BrandService brandService;
+
+    @Autowired
+    public BrandsController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @GetMapping("/getall")
+    public List<GetAllBrandsResponse> getAll(){
+        //IoC
+        return brandService.getAll();
+    }
+
+    @PostMapping("/add")
+    public void add(@RequestBody CreateBrandRequest createBrandRequest){
+        this.brandService.add(createBrandRequest);
+    }
+}
